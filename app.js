8e1b15e80e95d528a5432e5c1cb515c4a5a98657
Udit Agarwal <hello@uditagarwal.com>
2016-03-04 15:53:09 +0530

    Add methods to send and receive messages via websockets

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 
-//var scheme   = "ws://";
-//var uri      = scheme + "172.16.44.96:5000" + "/";
-//var ws       = new WebSocket(uri);
 //ws.onmessage = function(message) {
 //    var data = JSON.parse(message.data);
 //    $("#chat-text").append("<div class='panel panel-default'><div class='panel-heading'>" + data.handle + "</div><div class='panel-body'>" + data.text + "</div></div>");
@@ -17,35 +14,74 @@
 //    ws.send(JSON.stringify({ handle: handle, text: text }));
 //    $("#input-text")[0].value = "";
 //});
-var namespaceID = "flockster-";
-
-function newDivWithID(id) {
-    return $("<div>").attr("id", id);
-}
-
-function newDivWithClass(divClass) {
-    return $("<div>").addClass(divClass);
-}
-
-function showStartChatButton(id) {
-    var plusDiv = newDivWithID(id).html("+");
-    $("body").append(plusDiv);
-}
-
-function showChatPopUp(chatPopUpID) {
-    console.log("start chat with popup ID: " + chatPopUpID);
-    var chatPopUp = newDivWithID(chatPopUpID);
-
-    var chatPopUpHeader = newDivWithClass(namespaceID + "header");
-    var chatPopUpContent = newDivWithClass(namespaceID + "content");
-    var chatPopUpFooter = newDivWithClass(namespaceID + "footer");
-
-    chatPopUp.append(chatPopUpHeader, chatPopUpContent, chatPopUpFooter);
-
-    $("body").append(chatPopUp);
-}
 
 $(document).ready(function() {
+    var namespaceID = "flockster-";
+
+    function newDivWithID(id) {
+        return $("<div>").attr("id", id);
+    }
+
+    function newDivWithClass(divClass) {
+        return $("<div>").addClass(divClass);
+    }
+
+    function showStartChatButton(id) {
+        var plusDiv = newDivWithID(id).html("+");
+        $("body").append(plusDiv);
+    }
+
+    function showChatPopUp(chatPopUpID) {
+        console.log("start chat with popup ID: " + chatPopUpID);
+        var chatPopUp = newDivWithID(chatPopUpID);
+
+        var chatPopUpHeader = newDivWithClass(namespaceID + "header");
+        var chatPopUpContent = newDivWithClass(namespaceID + "content");
+        var chatPopUpFooter = newDivWithClass(namespaceID + "footer");
+
+        chatPopUp.append(chatPopUpHeader, chatPopUpContent, chatPopUpFooter);
+
+        $("body").append(chatPopUp);
+    }
+
+    var ws = null;
+    var token = null;
+
+    function initWebSocket() {
+        var scheme   = "ws://";
+        var uri      = scheme + "172.16.44.96:5000" + "/";
+        return new WebSocket(uri);
+    }
+
+    function startChat(chatPopUpID) {
+        ws = initWebSocket();
+        ws.onmessage = function(message) {
+            var data = JSON.parse(message.data);
+            receivedMessage(data);
+        };
+    }
+
+    function showMessageOnScreen(message) {
+        $("#" + chatPopUpID + " .flockster-content").append("<div class='message'><span class='label'>" + message.token + "</span><span class='text'>" + message.text + "</span></div>");
+        //$("#chat-text").stop().animate({
+        //    scrollTop: $('#chat-text')[0].scrollHeight
+        //}, 800);
+    }
+
+    function receivedMessage(message) {
+        showMessageOnScreen(message);
+    }
+
+    function sendMessage(text, token) {
+        if(ws == null || token == null) {
+            //TODO: may be show some warning or error
+            return;
+        }
+        var message = { token: token, text: text };
+        ws.send(JSON.stringify(message));
+        showMessageOnScreen(message);
+    }
+
     var startChatID = namespaceID + "start-chat";
     showStartChatButton(startChatID);
 
@@ -53,5 +89,6 @@ $(document).ready(function() {
 
     $("#" + startChatID).on("click", function() {
         showChatPopUp(chatPopUpID);
+        startChat(chatPopUpID);
     });
 });
